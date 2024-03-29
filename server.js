49241866c5f7bf329578729c93177db1c7d44060
HemangVora <hemangvora2@gmail.com>
2024-03-30 02:40:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import cloudflareScraper from "cloudflare-scraper";
 puppeteer.use(StealthPlugin());
 import { exec } from "child_process";
 import { executablePath } from "puppeteer";
+import axios from "axios";
 const curlCommand = `curl 'https://core-api.prod.blur.io/v1/blend/active-liens/0xbd3531da5cf5857e7cfaa92426877b022e612cf8' \
   -H 'accept: */*' \
   -H 'accept-language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' \
@@ -44,134 +45,117 @@ const ApiResponseSchema = new mongoose.Schema({
   },
 });
 const ApiResponse = mongoose.model("ApiResponse", ApiResponseSchema);
-
-const options = {
-  headers: {
-    Accept: "*/*",
-
-    "accept-Encoding": "gzip, deflate, br, zstd",
-    "content-type": "text/plain",
-    "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
-    cookie:
-      "rl_page_init_referrer=RudderEncrypt%3AU2FsdGVkX1%2FlSN88%2BDLogGHmJLm4uIIkjBymBKbcc9vGzFUcfGam3yaWO0b3HYyM; rl_page_init_referring_domain=RudderEncrypt%3AU2FsdGVkX1%2BgVViISIN3neKvaNS5pn11uMdCiHRNwpg%3D; _ga=GA1.1.482897642.1710265645; _ga_C09NSBBFNH=GS1.1.1710265645.1.1.1710265779.0.0.0; fs_uid=#o-19FFTT-na1#9fa28098-c277-451d-a0d8-b9cbef6ee45b:deeeafd5-a9d4-4907-b240-1f5f3d654712:1710265719842::2#/1734245244; rl_user_id=RudderEncrypt%3AU2FsdGVkX187euB47QC1lMLMzo5X0%2By8QiIUY8CK%2B18%3D; rl_trait=RudderEncrypt%3AU2FsdGVkX18fEHrHZfnkoFxFeVOmxlHnZlzBCT%2FQjs4%3D; rl_group_id=RudderEncrypt%3AU2FsdGVkX19UkqmVtTWkIUB17pIwcT0DnbRKUWX%2FEQk%3D; rl_group_trait=RudderEncrypt%3AU2FsdGVkX1%2BbsUn2Sy7fU6WSgmMtE5XyWwySKTyNTrU%3D; rl_anonymous_id=RudderEncrypt%3AU2FsdGVkX19VsAtYGF7g6CUoUuWY0kgXWsgrqzNbHcKUad37E0zGpkmZdZyS63mZIKBNSElgmAM8MLd2qSljPg%3D%3D; __cf_bm=YOU9LUwlvn55iGk5zvZHYkfK_2OgXcfyPmVk_yrBWg8-1711615149-1.0.1.1-csqflB9bk_BfaTaOWG545xHgwlurkGVzWxIpodjX.QjcVQLpPrE.eEtPrNMGi6mE99J9jUvJjFxs5nLnp2SKMw; rl_session=RudderEncrypt%3AU2FsdGVkX19uq0qjdt5WZBdoHDBPuvgmOgxEQuxZcuIeXukd%2FQN4M56aJ6Eack7oxMGT3T2FH%2BmnSmiKaH%2FeV8IM5mxWDcB0zVtt%2FXF938Z0bAe4CiPajuutOPpLHetspfTCuMHIiRe%2Fe27jb0mQfA%3D%3D",
-    "if-none-match": 'W/"5c028-o4ZnxrSRIjtRGGcDnFI7naMthK8"',
-    origin: "https://blur.io/",
-    referer: "https://blur.io/",
-    "sec-ch-ua":
-      '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"macOS"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "User-Agent":
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+const ApiResponseZenrows = mongoose.model(
+  "ApiResponseZenrows",
+  ApiResponseSchema
+);
+const ApiResponseScrapingAnt = mongoose.model(
+  "ApiResponseScrapingAnt",
+  ApiResponseSchema
+);
+const ApiMetrixSchema = new mongoose.Schema({
+  success_req_count: mongoose.Schema.Types.String,
+  name: mongoose.Schema.Types.String,
+  total_req_count: mongoose.Schema.Types.String,
+  status_code_arr: mongoose.Schema.Types.Array,
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
-  cloudflareTimeout: 5000,
-  // Reduce Cloudflare's timeout to cloudflareMaxTimeout if it is excessive
-  cloudflareMaxTimeout: 30000,
-  // followAllRedirects - follow non-GET HTTP 3xx responses as redirects
-  followAllRedirects: true,
-  // Support only this max challenges in row. If CF returns more, throw an error
-  challengesToSolve: 3,
-  // Remove Cloudflare's email protection, replace encoded email with decoded versions
-  decodeEmails: false,
-  // Support gzip encoded responses (Should be enabled unless using custom headers)
-  gzip: true,
-
-  // uri: "https://104.18.10.49:443/v1/blend/active-liens/0xbd3531da5cf5857e7cfaa92426877b022e612cf8",
-  url: "https://core-api.prod.blur.io/v1/blend/active-liens/0xbd3531da5cf5857e7cfaa92426877b022e612cf8",
-  method: "GET",
-  timeout: {
-    send: 5000,
-  },
-};
+});
+const ApiMetrix = mongoose.model("ApiMetrix", ApiMetrixSchema);
 
 async function fetchDataAndSave() {
-  try {
-    exec(curlCommand, async (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
+  console.log("starting to execute zenrows");
+  const url =
+    "https://core-api.prod.blur.io/v1/blend/active-liens/0xbd3531da5cf5857e7cfaa92426877b022e612cf8";
+  const apikey = "c77bc7ac5fa9dd3d8bb63e1f9b304c2e66f72940";
+  axios({
+    url: "https://api.zenrows.com/v1/",
+    method: "GET",
+    params: {
+      url: url,
+      apikey: apikey,
+      autoparse: "true",
+    },
+  })
+    .then(async (response) => {
+      console.log(response.status);
+      const apiResponse = new ApiResponseZenrows({ data: response.data });
+      await apiResponse.save();
+
+      let zenrow = await ApiMetrix.findOne({ name: "zenrows" });
+      if (!zenrow) {
+        const ApiMetrixresp = new ApiMetrix({
+          name: "zenrows",
+          success_req_count: "1",
+          total_req_count: "1",
+          status_code_arr: [{ code: response.status, id: "1" }],
+        });
+        await ApiMetrixresp.save();
+      } else {
+        zenrow.total_req_count = parseInt(zenrow.total_req_count) + 1;
+        zenrow.success_req_count = parseInt(zenrow.success_req_count) + 1;
+        zenrow.status_code_arr.push({
+          code: response.status,
+          id: zenrow.total_req_count,
+        });
+        await zenrow.save();
       }
-      if (stderr) {
-        console.log(`Response: ${stdout}`);
-        let apiresp = JSON.parse(stdout);
-        const apiResponse = new ApiResponse({ data: apiresp });
-        await apiResponse.save();
-        console.log("Data saved to MongoDB");
-        return;
-      }
-      // Output the response from your curl command
-      console.log(`Response: ${stdout}`);
+      console.log("zenrows executed successfully");
+    })
+    .catch(async (error) => {
+      console.log(error);
+      let zenrow = await ApiMetrix.findOne({ name: "zenrows" });
+      zenrow.total_req_count = parseInt(zenrow.total_req_count) + 1 + "";
+      await zenrow.save();
     });
 
-    // *********************
-    // PUPPETEER CODE START
-    // *********************
-    // const browser = await puppeteer.launch({
-    //   args: [
-    //     "--disable-setuid-sandbox",
-    //     "--no-sandbox",
-    //     "--single-process",
-    //     "--no-zygote",
-    //   ],
-    //   executablePath:
-    //     process.env.NODE_ENV === "production"
-    //       ? process.env.PUPPETEER_EXECUTABLE_PATH
-    //       : puppeteer.executablePath(),
-    // });
-    // try {
-    //   const page = await browser.newPage();
+  console.log("starting to execute ScrapingAnt");
+  const options = {
+    method: "GET",
+    url: "https://scrapingant.p.rapidapi.com/get",
+    params: {
+      url: "https://core-api.prod.blur.io/v1/blend/active-liens/0xbd3531da5cf5857e7cfaa92426877b022e612cf8",
+    },
+    headers: {
+      "X-RapidAPI-Key": "d627baa430msh0cea6b635744b8fp122576jsnfee27dd06dda",
+      "X-RapidAPI-Host": "scrapingant.p.rapidapi.com",
+    },
+  };
 
-    //   console.log("we are reasy to rock and roll");
+  try {
+    const response = await axios.request(options);
+    const apiResponse1 = new ApiResponseScrapingAnt({ data: response.data });
+    await apiResponse1.save();
 
-    //   await page.setViewport({
-    //     width: 1920 + Math.floor(Math.random() * 100),
-    //     height: 3000 + Math.floor(Math.random() * 100),
-    //     deviceScaleFactor: 1,
-    //     hasTouch: false,
-    //     isLandscape: false,
-    //     isMobile: false,
-    //   });
-    //   await page.goto(
-    //     "https://core-api.prod.blur.io/v1/blend/active-liens/0xbd3531da5cf5857e7cfaa92426877b022e612cf8"
-    //   );
-    //   console.log("this 1");
-    //   await new Promise((resolve) => setTimeout(resolve, 10000));
-    //   console.log("this");
-
-    //   // Extracting JSON data directly
-
-    //   const jsonContent = await page.evaluate(() => document.body.innerText);
-
-    //   // Optionally, parse the JSON to ensure it's valid and perhaps to manipulate it before saving
-    //   console.log(jsonContent);
-    //   const jsonData = JSON.parse(jsonContent);
-    //   console.log(jsonData);
-    //   console.log("JSON data has been saved.");
-    // } catch (e) {
-    //   console.error(e);
-    //   res.send(`Something went wrong while running Puppeteer: ${e}`);
-    // } finally {
-    //   await browser.close();
-    // }
-    // *********************
-    // PUPPETEER CODE END
-    // *********************
-
-    // cloudscraper(options)
-    //   .then(async function (data) {
-    //     const apiResponse = new ApiResponse({ data: data });
-    //     await apiResponse.save();
-    //     console.log("Data saved to MongoDB");
-    //   })
-    //   .catch(function (err) {
-    //     console.log(err);
-    //   });
+    let ScrapingAnt = await ApiMetrix.findOne({ name: "ScrapingAnt" });
+    if (!ScrapingAnt) {
+      const ApiMetrixresp = new ApiMetrix({
+        name: "ScrapingAnt",
+        success_req_count: "1",
+        total_req_count: "1",
+        status_code_arr: [{ code: response.status, id: "1" }],
+      });
+      await ApiMetrixresp.save();
+    } else {
+      ScrapingAnt.total_req_count = parseInt(ScrapingAnt.total_req_count) + 1;
+      ScrapingAnt.success_req_count =
+        parseInt(ScrapingAnt.success_req_count) + 1;
+      ScrapingAnt.status_code_arr.push({
+        code: response.status,
+        id: ScrapingAnt.total_req_count,
+      });
+      await ScrapingAnt.save();
+    }
   } catch (error) {
-    console.error("Error:", error);
+    console.error(error);
+    let ScrapingAnt = await ApiMetrix.findOne({ name: "ScrapingAnt" });
+    ScrapingAnt.total_req_count =
+      parseInt(ScrapingAnt.total_req_count) + 1 + "";
+    await ScrapingAnt.save();
   }
+  console.log("ScrapingAnt executed successfully");
 }
 
 app.get("/get", async (req, res) => {
@@ -179,8 +163,8 @@ app.get("/get", async (req, res) => {
   // console.log(apiResponse);
   return res.json({ apiResponse });
 });
-// Call fetchDataAndSave every 5 minutes
-setInterval(fetchDataAndSave, 5 * 60 * 1000);
+// Call fetchDataAndSave every 15 minutes
+setInterval(fetchDataAndSave, 15 * 60 * 1000);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
