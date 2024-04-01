4481527c070dd46d64acd74679322d6434e528ba
HemangVora <hemangvora2@gmail.com>
2024-04-01 23:50:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
-    // fetchDataAndSave();
+    fetchDataAndSave();
   })
   .catch((err) => console.error("MongoDB connection error:", err));
 
